02cfa3e488f4ad0851f7dc5fc8f948d1be6a6c15
丁福红 <3085423437@qq.com>
2024-05-09 09:04:23 +0800

    仓库初始化

---
+++ pages/home/home.vue
@@ -0,0 +1,60 @@
+<template>
+		<view class="one">
+			888888
+		</view>
+	<view class="two">
+		12345
+	</view>
+</template>
+
+<script setup>
+	// export default {
+	// 	data() {
+	// 		return {
+	// 			title: 'Hello'
+	// 		}
+	// 	},
+	// 	onLoad() {
+
+	// 	},
+	// 	methods: {
+
+	// 	}
+	// }
+</script>
+
+<style lang="scss">
+	.one{
+		color: seagreen;
+		font-size: 24rpx;
+	}
+	.two{
+		color: mediumpurple;
+		font-size: 10rpx;
+	}
+	/* .content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	} */
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/home/home.js
@@ -0,0 +1,12 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  __name: "home",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return {};
+    };
+  }
+};
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "D:/桌面/Desktop/bscode/wxcode/pages/home/home.vue"]]);
+wx.createPage(MiniProgramPage);
