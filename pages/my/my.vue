2e6f26720dec6dfc134ea95bdfaebf279259e62f
yyl <1469744280@qq.com>
2024-05-18 13:41:01 +0800

    feat: 我的&我的订单页面

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,78 +1,114 @@
 <template>
-<!-- 头像+用户名称 -->
-	<view class="proImage">
-		<!-- 用户头像 -->
-		<uni-text data-v-2ddb064c="" class="lg text-white cuIcon-profile" ><span></span></uni-text>
-		<!-- 微信用户名称 -->
-		<view class="wxName">用户名</view>
-	</view>
-	
-	<view class="itemKinds">
-		<!-- 我的金额 -->
-			
-			
-		<!-- 我的订单 -->
-			
-			
-		<!-- 帮助中心 -->
-		
-	</view>
-
-
-
-
+  <!-- 头像+用户名称 -->
+  <view class="proImage bg-blue">
+    <!-- 用户头像 -->
+    <uni-text data-v-2ddb064c="" class="lg text-white cuIcon-profile"
+      ><span></span
+    ></uni-text>
+    <!-- 微信用户名称 -->
+    <view class="wxName">用户名</view>
+  </view>
+  <!-- 我的金额+我的订单+帮助中心  -->
+  <view class="content">
+    <view class="content_main">
+      <view
+        v-for="item in contentList"
+        :key="item.id"
+        class="content_main_item flex bg-white"
+        @click="handleClick(item.id)"
+      >
+        <view class="flex item-center">
+          <text style="font-weight: 500">{{ item.value }} </text>
+          <text
+            v-if="item.id === 1"
+            class="text-blue"
+            style="margin-left: 20rpx"
+          >
+            ¥ 0.99</text
+          >
+        </view>
+        <text v-if="item.id === 1" class="text-blue">充值</text>
+        <text v-else class="cuIcon-right"> </text>
+      </view>
+    </view>
+  </view>
 </template>
 
-<script>
-	// 导入bottomButton.vue组件
-	// export default {
-	// 	data() {
-	// 		return {
-
-	// 		}
-	// 	},
-	// 	methods: {
-
-	// 	}
-	// }
+<script setup>
+const contentList = [
+  {
+    id: 1,
+    value: "我的金额",
+  },
+  {
+    id: 2,
+    value: "我的订单",
+  },
+  {
+    id: 3,
+    value: "帮助中心",
+  },
+];
+const handleClick = (id) => {
+  if (id === 1) {
+    uni.navigateTo({
+      url: "/pages/my/myAmount",
+    });
+  } else if (id === 2) {
+    uni.navigateTo({
+      url: "/pages/my/myOrder",
+    });
+  }
+};
 </script>
 
-<style>
-	/* 头像+用户名称 */
-	.proImage {
-		display: flex;
-		/* position: absolute; */
-		background-color: aquamarine;
-		height: 180rpx;
-		width: 100%;
-		padding-top: 40rpx;
-		padding-left: 8%;
-	}
-	/* 头像 */
-	.cuIcon-profile{
-		font-size: -webkit-xxx-large
-	}
-	/* 用户名称 */
-	.wxName {
-		/* position: relative; */
-		float: left;
-		padding-top: 20rpx;
-		padding-left: 2%;
-		font-size: 40rpx;
-		color: white;
-	}
-	
-	/* 我的金额+我的订单+帮助中心 */
-	.itemKinds {
-		background-color: orchid;
-		height: 630rpx;
-		width: 100%;
-	}
-	/* 我的金额 */
-	
-	/* 我的订单 */
-	
-	/* 帮助中心 */
-	
-	
+<style lang="scss" scoped>
+/* 头像+用户名称 */
+.proImage {
+  display: flex;
+  position: relative;
+  /* background-color: aquamarine; */
+  height: 260rpx;
+  width: 100%;
+  padding-top: 40rpx;
+  padding-left: 8%;
+}
+
+/* 头像 */
+.cuIcon-profile {
+  font-size: -webkit-xxx-large;
+}
+
+/* 用户名称 */
+.wxName {
+  /* position: relative; */
+  float: left;
+  padding-top: 20rpx;
+  padding-left: 2%;
+  font-size: 40rpx;
+  color: white;
+}
+
+/* 我的金额+我的订单+帮助中心 */
+.content {
+  position: absolute;
+  background-color: #f1f1f1;
+  border-radius: 50rpx 50rpx 0 0;
+  height: 630rpx;
+  width: 100%;
+  top: 180rpx;
+  &_main {
+    margin-top: 60rpx;
+    &_item {
+      box-sizing: border-box;
+      height: 160rpx;
+      width: 98%;
+      line-height: 160rpx;
+      padding: 0 50rpx;
+      margin: 15rpx 5rpx;
+      justify-content: space-between;
+      border-radius: 10rpx;
+    }
+  }
+}
 </style>
---
+++ pages/my/myOrder.vue
@@ -0,0 +1,70 @@
+<template>
+  <view class="order-list">
+    <view
+      v-for="item in orderList"
+      :key="item.id"
+      class="order-list_item bg-white"
+    >
+      <view>
+        <view class="text-lg">{{ item.equipName }}</view>
+        <view>{{ item.createTime }}</view>
+      </view>
+      <view>
+        <view>用水量</view>
+        <view>{{ item.waterConsumption }}</view>
+      </view>
+      <view>
+        <view>水量预警</view>
+        <view>{{ item.waterWarning }}</view>
+      </view>
+      <view>
+        <view>用水金额</view>
+        <view>{{ item.waterCost }}</view>
+      </view>
+    </view>
+  </view>
+</template>
+<script setup>
+const orderList = [
+  {
+    id: 1,
+    equipName: "设备01",
+    createTime: "2024/05/16 16:00",
+    waterConsumption: "2.5L",
+    waterWarning: "1.5L",
+    waterCost: "¥ 2.00",
+  },
+  {
+    id: 2,
+    equipName: "设备02",
+    createTime: "2024/05/18 18:00",
+    waterConsumption: "3.5L",
+    waterWarning: "1.0L",
+    waterCost: "¥ 3.50",
+  },
+];
+</script>
+<style lang="scss" scoped>
+.order-list {
+  &_item {
+    display: flex;
+    padding: 30rpx 10rpx;
+    justify-content: space-around;
+    margin-top: 20rpx;
+  }
+  &_item > view:nth-child(2),
+  &_item > view:nth-child(3),
+  &_item > view:nth-child(4) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  &_item > view:first-child {
+    text-align: left;
+  }
+  &_item > view view:nth-child(2) {
+    margin-top: 16rpx;
+    color: #9ca3af;
+  }
+}
+</style>
